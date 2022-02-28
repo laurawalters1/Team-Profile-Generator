4be1c71530d592fb5c8d1960ba2e7eeb4d189867
Laura Walters <laura.walters1098@yahoo.com>
2022-02-28 23:22:09 +0000

    styling index

--- index.js
+++ index.js
@@ -127,7 +127,27 @@ addAnotherEmployeePrompt = () => {
       } else {
         console.log("You chose to finish building your team profile!");
         const stringCards = employeesCardsArr.toString();
-        fs.writeFile("./dist/index.js", stringCards, function (err, result) {
+        const html = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <title>Team Profile</title>
+        </head>
+        <body>
+          <header class="text-center p-5">
+            <h1>Team Profile</h1>
+          </header>
+          <main class="d-flex justify-content-center w-100">
+        ${stringCards}
+        </main>
+        </body>
+        </html> `;
+
+        fs.writeFile("./dist/index.html", html, function (err, result) {
           if (err) {
             console.log("There was an error writing the file!");
           }
