9f0aa2f73efdef4535ab8b9022bf7d35ea364112
Laura Walters <laura.walters1098@yahoo.com>
2022-02-28 20:24:51 +0000

    improving employee card functions

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const fs = require("fs");
+const Employee = require("./lib/Employee.js");
+const Manager = require("./lib/Manager.js");
+// const Engineer = require("./lib/Engineer.js");
+// const Intern = require("./lib/Intern.js");
+
 console.log("hello");
+
+// const roles = ["Manager", "Engineer", "Intern", "Finish Building application"];
+
+// if (answers.role === "manager") {
+//   const manager = new Manager(answers.name, answers.id, inswers.officeNumber);
+// }
+
+// employees = [];
+
+fs.writeFile("./dist/index.js", "hello", function (err, result) {
+  if (err) {
+    console.log("There was an error");
+  }
+});
